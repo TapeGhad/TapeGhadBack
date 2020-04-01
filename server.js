e4a1d196c01d0039cca1b471f9282ef797a51a2b
tapeghad <tapeghad@gmail.com>
2020-04-01 18:40:55 +0300

    deploy

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT;
 
 const usersRouter = require('./routes/users');
 const CollRouter = require('./routes/collections');
