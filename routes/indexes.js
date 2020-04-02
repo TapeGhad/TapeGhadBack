81801ad811ceb906ca5f4baa3e40c5163d6ddc05
tapeghad <tapeghad@gmail.com>
2020-04-02 18:41:56 +0300

    deploy

--- routes/indexes.js
+++ routes/indexes.js
@@ -2,12 +2,12 @@ const router = require('express').Router();
 const Item = require('../models/item.model');
 const Collection = require("../models/collection.model")
 
-router.route('/add/collection').post((req, res) => {
+router.route('/add/collection').get((req, res) => {
     Collection.createIndex({description: "text", topic: "text"}, {"weights": {description: 1, topic: 2}}).then(ok => res.json("Ok"))
     .catch(err => res.json('Some error, try later111'));
 });
 
-router.route('/add/item').post((req, res) => {
+router.route('/add/item').get((req, res) => {
     Item.createIndex({collectionName: "text", name: "text", tags: "text", comments:"text"}, {"weights": {collectionName: 1, name: 2, tags: 3, comments: 4}}).then(ok => res.json("Ok"))
     .catch(err => res.json('Some error, try later111'));
 });
